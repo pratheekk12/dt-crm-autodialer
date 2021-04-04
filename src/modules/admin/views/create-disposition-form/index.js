6fa84a10a39c6a4c25ecaf38b1d87c4bce9f1be3
Deepak <drons9991@gmail.com>
2021-04-04 13:41:30 +0530

    form builder css fixed

--- src/modules/admin/views/create-disposition-form/index.js
+++ src/modules/admin/views/create-disposition-form/index.js
@@ -1,57 +1,73 @@
-import {
-  Button, Card, Grid,
-} from '@material-ui/core';
+import { Button, Card, Grid } from '@material-ui/core';
 import { Form, Formik } from 'formik';
-import React  from 'react';
+import React from 'react';
 import DispositionQuestions from './DispositionQuestions';
 import SortableTree from 'react-sortable-tree';
 import 'react-sortable-tree/style.css';
-import { createNodeForSortableTree } from './utils'
+import { createNodeForSortableTree } from './utils';
 
 const initialValues = {
-  dispositionQuestions: [{
-    "question": "",
-    option: [
-      {
-        label: '',
-      },
-    ],
-  }]
-}
+  dispositionQuestions: [
+    {
+      question: '',
+      option: [
+        {
+          label: ''
+        }
+      ]
+    }
+  ]
+};
 
 function CreateDispositionForm() {
-  const [sTree, setSTree] = React.useState(createNodeForSortableTree(initialValues.dispositionQuestions));
+  const [sTree, setSTree] = React.useState(
+    createNodeForSortableTree(initialValues.dispositionQuestions)
+  );
 
-  return (<Grid container direction="row" justify="center" spacing={3} style={{ backgroundColor: 'white', margin: '1rem', padding: '2rem', display: 'contents'  }}>
+  return (
+    <div
+      style={{
+        margin: '1.5rem'
+      }}
+    >
+      <Grid container direction="row" justify="center" spacing={3}>
         <Grid item lg={7} xs={12}>
-          <Card style={{ display: 'flex', justifyContent: 'center', padding: 10 }}>
+          <div style={{ backgroundColor: 'white', padding: '0.5rem' }}>
             <Formik
               initialValues={initialValues}
-              onSubmit={async (values) => {
-                createNodeForSortableTree(values.dispositionQuestions)
-                setSTree(values.dispositionQuestions)
+              onSubmit={async values => {
+                createNodeForSortableTree(values.dispositionQuestions);
+                setSTree(values.dispositionQuestions);
               }}
               validateOnChange={false}
             >
               {({ values }) => (
                 <Form>
-                  <DispositionQuestions questions={values.dispositionQuestions} name={"dispositionQuestions"}/>
+                  <DispositionQuestions
+                    questions={values.dispositionQuestions}
+                    name={'dispositionQuestions'}
+                  />
                   <Button variant="contained" color="primary" type="submit">
                     {'Save'}
                   </Button>
                 </Form>
               )}
             </Formik>
-          </Card>
-          <Card style={{ padding: 10 }}>
-            <div style={{ height: 400 }}>
-              <SortableTree
-                treeData={sTree}
-              />
-            </div>
-          </Card>
+          </div>
+        </Grid>
+        <Grid item lg={5} xs={12}>
+          <div
+            style={{
+              height: 400,
+              backgroundColor: 'white',
+              marginBottom: '1.5rem'
+            }}
+          >
+            <SortableTree treeData={sTree} />
+          </div>
         </Grid>
       </Grid>
+    </div>
   );
 }
 
