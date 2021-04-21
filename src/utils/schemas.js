b7a27f3b8791037009b4df09813b4fdd7c0d4d0a
jatinchhabra-1 <jatinc35@gmail.com>
2021-04-21 18:43:02 +0530

    Schema Updated

--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -20,7 +20,25 @@ export const RegistrationSchema = admin =>
       .string()
       .required(errorStrings.ERR_REQ)
       .email(),
-    role: admin ? yup.string().required(errorStrings.ERR_REQ) : yup.string()
+    role: admin ? yup.string().required(errorStrings.ERR_REQ) : yup.string(),
+    restaurants: admin
+      ? yup.array().when('role', (data, schema) => {
+          return data === 'DTAM'
+            ? schema
+                .min(1, 'Minimum Length Required is 1')
+                .max(100, 'Max Length allowed is 100')
+            : data !== 'DTL1'
+            ? schema.min(1, 'Minimum Length Required is 1').max(1, 'Maximum Length Allowed is 1')
+            : schema.min(0, 'No Restaurants can be assigned').max(0, 'No Restaurants can be assigned');
+          // return data.Dropdown1 === 'First'
+          //   ? schema2.required('this is a required field')
+          //   : schema2;
+        })
+      : // .min(8)
+        // .test('passwords-match', 'Passwords must match', function(value) {
+        //   return this.parent.password === value;
+        // })
+        null
   });
 
 export const PasswordSchema = yup.object({
