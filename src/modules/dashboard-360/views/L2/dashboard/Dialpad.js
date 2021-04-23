6dfa938d5e5940148124ca180b5fb68344284c07
Deepak <drons9991@gmail.com>
2021-04-23 12:40:46 +0530

    diapad added

--- src/modules/dashboard-360/views/L2/dashboard/Dialpad.js
+++ src/modules/dashboard-360/views/L2/dashboard/Dialpad.js
@@ -1,23 +1,25 @@
-import React from 'react';
-
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from 'react';
+import { Formik, Form, Field } from 'formik';
 import Popover from '@material-ui/core/Popover';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import { IconButton, TextField } from '@material-ui/core';
+import { IconButton, Snackbar } from '@material-ui/core';
 import PhoneIcon from '@material-ui/icons/Phone';
 import axios from 'axios';
+import { TextField } from 'formik-material-ui';
+import * as Yup from 'yup';
+import MuiAlert from '@material-ui/lab/Alert';
 
-const useStyles = makeStyles(theme => ({
-  typography: {
-    padding: theme.spacing(2)
-  }
-}));
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 export default function Dialpad({ agentPhoneNumber }) {
-  const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const [phoneNumber, setPhoneNumber] = React.useState(0);
+  const [snackbarOpen, setSnackbarOpen] = React.useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState({
+    severity: '',
+    message: ''
+  });
 
   const handleClick = event => {
     setAnchorEl(event.currentTarget);
@@ -27,21 +29,35 @@ export default function Dialpad({ agentPhoneNumber }) {
     setAnchorEl(null);
   };
 
-  const dial = async () => {
-    await axios.get('https://dt.granalytics.in/ami/actions/orginatecall', {
-      params: {
-        sipAgentID: `Local/2${agentPhoneNumber}@from-internal`,
-        NumbertobeCalled: '2' + phoneNumber
-      }
-    });
+  const handleSnackbarClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnackbarOpen(false);
   };
 
-  const handleCall = () => {
-    if (phoneNumber.length === 10) {
-      dial();
-    } else {
-      alert('Please enter valid phone number');
-    }
+  const dial = async phoneNumber => {
+    await axios
+      .get('https://dt.granalytics.in/ami/actions/orginatecall', {
+        params: {
+          sipAgentID: `Local/2${agentPhoneNumber}@from-internal`,
+          NumbertobeCalled: '2' + phoneNumber
+        }
+      })
+      .then(res => {
+        setSnackbarOpen(true);
+        setSnackbarMessage({
+          severity: 'success',
+          message: `Call connecting !`
+        });
+      })
+      .catch(err => {
+        setSnackbarOpen(true);
+        setSnackbarMessage({
+          severity: 'error',
+          message: `Something went wrong. Please try again !`
+        });
+      });
   };
 
   const open = Boolean(anchorEl);
@@ -72,20 +88,49 @@ export default function Dialpad({ agentPhoneNumber }) {
         }}
       >
         <div style={{ margin: '0.5rem' }}>
-          <TextField
-            id="outlined-number"
-            label="Phone Number"
-            type="number"
-            variant="outlined"
-            onChange={e => {
-              setPhoneNumber(e.target.value);
+          <Formik
+            initialValues={{ phone: '' }}
+            validationSchema={Yup.object({
+              phone: Yup.string()
+                .matches(
+                  /^((\+){0,1}91(\s){0,1})?([6-9]\d{9})$/,
+                  'Enter valid Phone Number'
+                )
+                .required('Required')
+            })}
+            onSubmit={(values, { resetForm }) => {
+              dial(values.phone);
+              resetForm();
             }}
-          />
-          <IconButton color="primary" aria-label="phone" onClick={handleCall}>
-            <PhoneIcon />
-          </IconButton>
+          >
+            <Form>
+              <Field
+                component={TextField}
+                label="Phone Number"
+                name="phone"
+                type="number"
+                variant="outlined"
+              />
+              <IconButton color="primary" aria-label="phone" type="submit">
+                <PhoneIcon />
+              </IconButton>
+            </Form>
+          </Formik>
         </div>
       </Popover>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={4000}
+        onClose={handleSnackbarClose}
+        style={{ width: '100%' }}
+      >
+        <Alert
+          onClose={handleSnackbarClose}
+          severity={snackbarMessage.severity}
+        >
+          {snackbarMessage.message}
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
